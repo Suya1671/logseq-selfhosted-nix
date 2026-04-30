685a235b5e62d8893892353eb60c6a5e0587190b
Suya Singh <suya1671@tuta.io>
2026-04-30 23:57:23 +0200

    chore: replace devenv with regular nix devshell

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     nvfetcher = {
       url = "github:berberman/nvfetcher";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,15 +12,6 @@
       url = "github:jlesquembre/clj-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix2container = {
-      url = "github:nlewo/nix2container";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    mk-shell-bin = {
-      url = "github:rrbutani/nix-mk-shell-bin";
-    };
   };
 
   outputs = inputs@{ flake-parts, nixpkgs, ... }:
@@ -33,19 +19,15 @@
       ...
     }: {
       imports = [
-        inputs.devenv.flakeModule
         ./nix/packages
         ./nix/modules
         ./nix/checks.nix
       ];
       systems = nixpkgs.lib.systems.flakeExposed;
 
-      perSystem = { ... }: {
-        devenv.shells.default = {
-          languages.nix = {
-            enable = true;
-            lsp.enable = true;
-          };
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.nixd ];
         };
       };
     });
